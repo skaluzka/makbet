da146cfed0234a9df805052a7cc2313e7c3bda47
skaluzka <skaluzka+github@protonmail.com>
2020-07-15 21:04:22 +0200

    [#30] Add simple sleep task
    Add simple wrapper for "sleep" command/utility.

---
+++ lib/tasks/common/sleep.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu
+
+
+function main() {
+
+    local delay_in_seconds
+    delay_in_seconds="${1}"
+
+    sleep "${delay_in_seconds}"
+}
+
+main "$@"
+
+if (( "${MAKBET_VERBOSE}" >= 1 ))
+then
+    echo "Script ${0} completed."
+fi
+
+
+# The end
