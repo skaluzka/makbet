c70ff27e1a9a41aef1ca305e094253ea94771851
skaluzka <skaluzka+github@protonmail.com>
2020-12-30 00:13:48 +0100

    [DEV] Update few comments in makbet.mk

--- makbet.mk
+++ makbet.mk
@@ -195,7 +195,7 @@ ifeq ($(_v1), 1)
 endif
 
 #
-# Definition of TASK_template macro.
+# Define TASK_template macro.
 #
 # Where:
 # $(1) - TASK_NAME - The name of the task.
@@ -318,7 +318,9 @@ $(if $(_q),,
   )
 )
 
-# End of TASK_template macro.
+#
+# End of TASK_template macro definition.
+#
 endef
 
 
